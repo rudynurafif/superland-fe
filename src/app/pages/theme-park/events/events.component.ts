1038e5881070155b0e1a78441a958fb4d1c9a723
rudynurafif <rudynurafif@gmail.com>
2023-08-28 15:09:41 +0700

    preparing for merge

--- src/app/pages/theme-park/events/events.component.ts
+++ src/app/pages/theme-park/events/events.component.ts
@@ -8,16 +8,13 @@ import { Component } from '@angular/core';
 export class EventsComponent {
 
   ngAfterViewInit() {
-    // Fungsi untuk mengatur interval slide
     this.startCarousel();
   }
 
   startCarousel() {
-    // Mengakses elemen carousel
     const carouselElement: HTMLElement | null = document.querySelector('.carousel');
 
     if (carouselElement) {
-      // Memulai interval untuk mengganti slide setiap 3 detik
       setInterval(() => {
         const nextButton: HTMLButtonElement | null = carouselElement.querySelector('.carousel-control-next');
         if (nextButton) {
--- src/app/pages/theme-park/merchandise/merchandise.component.ts
+++ src/app/pages/theme-park/merchandise/merchandise.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class MerchandiseComponent {
 
+  cards = [
+    { title: 'Card title 1', imageUrl: '../../../../assets/images/t-shirt.png', description: 'Description for card 1.' },
+    { title: 'Card title 2', imageUrl: '../../.././../assets/images/tumbler.png', description: 'Description for card 2.' },
+    { title: 'Card title 3', imageUrl: '../../../../assets/images/t-shirt.png', description: 'Description for card 3.' },
+  ];
+
+  selectedCard: any = null;
+
+  openModal(card: any) {
+    this.selectedCard = card;
+  }
+
 }
