dccd524597c708fa6fa58e171303671334f80f33
rudynurafif <rudynurafif@gmail.com>
2023-08-31 19:29:30 +0700

    [Improve] improve some UI component after rebasing

--- src/app/pages/theme-park/our-team/our-team.component.ts
+++ src/app/pages/theme-park/our-team/our-team.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from "aos";
 
 @Component({
   selector: 'app-our-team',
@@ -6,6 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./our-team.component.scss']
 })
 export class OurTeamComponent {
-imgSrc= '/assets/images/test-card.png'
-  imgAlt: string= 'bg-1'
+  ngOnInit(){
+    AOS.init();
+  }
+
+  backgroundImageFauzan: string = "url('assets/images/our-team/ojan2.png')";
+  backgroundImageRudy: string = "url('assets/images/our-team/rudy.png')";
+  backgroundImageCilo: string = "url('assets/images/our-team/cilo.png')";
+
+  changeBackground(imageUrl: string, card: string) {
+    switch (card) {
+      case 'fauzan':
+        this.backgroundImageFauzan = `url('${imageUrl}')`;
+        break;
+      case 'rudy':
+        this.backgroundImageRudy = `url('${imageUrl}')`;
+        break;
+      case 'cilo':
+        this.backgroundImageCilo = `url('${imageUrl}')`;
+        break;
+      default:
+        break;
+    }
+    document.documentElement.style.setProperty('--hover-image', imageUrl);
+  }
+
+  resetBackground(card: string) {
+    switch (card) {
+      case 'fauzan':
+        this.backgroundImageFauzan = "url('assets/images/our-team/ojan2.png')";
+        break;
+      case 'rudy':
+        this.backgroundImageRudy = "url('assets/images/our-team/rudy.png')";
+        break;
+      case 'cilo':
+        this.backgroundImageCilo = "url('assets/images/our-team/cilo.png')";
+        break;
+      default:
+        break;
+    }
+    document.documentElement.style.setProperty('--hover-image', 'none');
+  }
 }
