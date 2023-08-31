729c32e3b2c220d40b369b2ac42af6e36c1cf19b
Susilo Yudayono <bamsusilo91@gmail.com>
2023-08-31 07:18:55 +0700

    [IMPROVE] make animation for home page and our-team page

--- src/app/pages/theme-park/our-team/our-team.component.ts
+++ src/app/pages/theme-park/our-team/our-team.component.ts
@@ -10,4 +10,44 @@ export class OurTeamComponent {
   ngOnInit(){
     AOS.init();
   }
+
+
+  backgroundImageFauzan: string = "url('assets/images/our-team/ojan2.png')";
+  backgroundImageRudy: string = "url('assets/images/our-team/rudy.png')";
+  backgroundImageCilo: string = "url('assets/images/our-team/cilo.png')";
+
+  changeBackground(imageUrl: string, card: string) {
+    switch (card) {
+      case 'fauzan':
+        this.backgroundImageFauzan = `url('${imageUrl}')`;
+        break;
+      case 'rudy':
+        this.backgroundImageRudy = `url('${imageUrl}')`;
+        break;
+      case 'cilo':
+        this.backgroundImageCilo = `url('${imageUrl}')`;
+        break;
+      default:
+        break;
+    }
+    document.documentElement.style.setProperty('--hover-image', imageUrl);
+  }
+
+  resetBackground(card: string) {
+    switch (card) {
+      case 'fauzan':
+        this.backgroundImageFauzan = "url('assets/images/our-team/ojan2.png')";
+        break;
+      case 'rudy':
+        this.backgroundImageRudy = "url('assets/images/our-team/rudy.png')";
+        break;
+      case 'cilo':
+        this.backgroundImageCilo = "url('assets/images/our-team/cilo.png')";
+        break;
+      default:
+        break;
+    }
+    document.documentElement.style.setProperty('--hover-image', 'none');
+  }
 }
+
